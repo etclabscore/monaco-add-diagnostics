37ce697aea523c5e1fefa2dbfc81d0bc15961005
shanejonas <jonas.shane@gmail.com>
2019-12-05 10:53:46 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Monaco Add JSON Schema Diagnostics
+
+When using multiple monaco editors, setting diagnostics for JSON Schemas from multiple projects/components/repositories is not straight forward. This modules goal is to provide an easy way to add JSON Schemas to the global diagnostics.
+
+### Contributing
+
+How to contribute, build and release are outlined in [CONTRIBUTING.md](CONTRIBUTING.md), [BUILDING.md](BUILDING.md) and [RELEASING.md](RELEASING.md) respectively. Commits in this repository follow the [CONVENTIONAL_COMMITS.md](CONVENTIONAL_COMMITS.md) specification.
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as monaco from "monaco-editor";
+
+interface IDiagnosticSchemaMap {
+  [k: string]: any;
+}
+
+const diagnosticSchemaMap: IDiagnosticSchemaMap = {};
+
+export function addDiagnostics(uri: string, schema: any): void {
+  diagnosticSchemaMap[uri] = schema;
+  monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+    enableSchemaRequest: true,
+    schemas: Object.values(diagnosticSchemaMap).map(([u, s]: [string, any]) => {
+      return {
+        fileMatch: ["*"],
+        schema: s,
+        uri: u,
+      };
+    }),
+    validate: true,
+  });
+}
